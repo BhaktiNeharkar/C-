dfcff219d5229268267575440c3c8486273a8a6f
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2023-09-29 01:50:52 +0530

    Add files via upload

---
+++ 6.AreaofCircle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+ int main()
+{
+    float r,area;
+    cout<<"Enter the radius";
+    cin>>r;
+    area=3.142*r*r;
+    cout<<"Area of Circle is "<<area;
+    return 0;
+}
