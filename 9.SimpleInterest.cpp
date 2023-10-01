e24fad50667c0874fab839aa258967aea8caf312
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2023-10-01 18:16:21 +0530

    Add files via upload

---
+++ 9.SimpleInterest.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    float v,r,h;
+    cout<<"Enter the required values for the Volume of Cylinder";
+    cin>>r>>h;
+    v=(3.142)*r*r*h;
+    cout<<"The Calculated value is "<<v;
+    return 0;
+    }
