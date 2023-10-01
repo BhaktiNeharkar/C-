fbbb0b14430715e2463781cb72ffcc6384b8c542
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2023-10-01 18:16:00 +0530

    Add files via upload

---
+++ 8.Distance.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{               
+    float d,t,s;
+    cout<<"Enter the values of speed and time ";
+    cin>>t>>s;
+    d=s*t;
+    cout<<"The Calculated Distance is "<<d;
+    return 0;
+}
