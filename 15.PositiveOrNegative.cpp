176e494d86c6849053d73db4835a630fb29f3108
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2023-10-04 16:15:55 +0530

    Add files via upload

---
+++ 15.PositiveOrNegative.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"Enter Two Numbers to Check Whether it is +ve or -ve ";
+    cin>>x;
+    if (x>=0)
+    {
+        cout<<"Entered No. is Positive"<<x;
+    }
+    else{
+        cout<<"Number is Negative ";
+    }
+     return 0;
+}
