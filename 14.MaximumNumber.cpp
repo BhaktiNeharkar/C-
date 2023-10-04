a03125399bd4904d797bf4aac1e215f6bf825a1f
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2023-10-04 16:15:18 +0530

    Add files via upload

---
+++ 14.MaximumNumber.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cout<<"Enter 2 no.s";
+    cin>>x>>y;
+    if (x>y)
+    {
+        cout<<"max. no. is "<<x;
+    }
+    else
+    {
+        cout<<"max. no. is "<<y;
+    }
+    return 0;
+}
