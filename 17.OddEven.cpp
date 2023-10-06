c8159c9b68180c2bffba59a9cb94ead8349ca3ae
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2023-10-06 06:47:38 +0530

    Add files via upload

---
+++ 17.OddEven.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"Enter A Number to check Even or Odd";
+    cin>>x;
+    if (x%2==0)
+    {
+        cout<<"The no. is Even";
+    }
+    else
+    {
+        cout<<"The no. is Odd";
+    }
+    return 0;
+}
