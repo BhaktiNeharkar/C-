7ce78e3b01f4628eef309bf98cd96b2b1274dc68
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2023-10-07 07:33:54 +0530

    Add files via upload

---
+++ 18.YoungOrNot.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout<<"Enter your age";
+    cin>>age;
+    if(age>12 && age<50)
+    cout<<"Young";
+    else 
+    cout<<"NOT Young";
+    return 0;
+}
