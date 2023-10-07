c4730bbfbcd8b3ebbcc35910657b8284c53f577e
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2023-10-07 07:34:13 +0530

    Add files via upload

---
+++ 19.Eligibility.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"Enter your age";
+    cin>>x;
+    if(x>18)
+      cout<<"Eligible to vote";
+      else
+      cout<<"NOT Eligible to vote";
+      return 0;
+}
