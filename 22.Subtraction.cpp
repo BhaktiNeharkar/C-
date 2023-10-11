f0b78eafcbfdd74c82db1eb13037371dc68049d0
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2023-10-11 22:50:52 +0530

    Add files via upload

---
+++ 22.Subtraction.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,sub;
+    cout<<"Enter 2 no.s";
+    cin>>a>>b;
+    sub=b-a;
+    cout<<"The subtraction is "<<sub;
+    return 0;
+}
