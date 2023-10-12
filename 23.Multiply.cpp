dfba05fabd29513ced2d63a81c65b7fb354f3c03
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2023-10-12 16:53:19 +0530

    Add files via upload

---
+++ 23.Multiply.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a,b,mult;
+    cout<<"Enter 2 numbers to multiply";
+    cin>>a>>b;
+    mult=a*b;
+    cout<<"Multiplication is "<<mult;
+    return 0;
+}
