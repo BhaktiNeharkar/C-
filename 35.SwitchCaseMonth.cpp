e1ff937f27986db0752eac7f34718de1ee12a600
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2023-10-20 23:10:36 +0530

    Add files via upload

---
+++ 35.SwitchCaseMonth.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cout<<"Enter a Number ";
+    cin>>number;
+    switch (number)
+{
+    case 1 :cout<<"JAN";
+    break;
+    case 2 : cout<<"FEB";
+    break;
+    case 3 : cout<<"MAR";
+    break;
+    case 4 : cout<<"APR";
+    break;
+    case 5 : cout<<"MAY";
+    break;
+    case 6 : cout<<"JUN";
+    break;
+    case 7 : cout<<"JUL";
+    break;
+    case 8 : cout<<"AUG";
+    break;
+    case 9 : cout<<"SEPT";
+    break;
+    case 10 : cout<<"OCT";
+    break;
+    case 11 : cout<<"NOV";
+    break;
+    case 12 : cout<<"DEC";
+    break;
+}
+return 0;
+}
