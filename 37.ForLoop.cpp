8a2b0b021d7c8a85350795060e93de31794e496e
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2023-10-25 23:23:05 +0530

    Add files via upload

---
+++ 37.ForLoop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,i=1;
+    cout<<"Enter the value of n ";
+    cin>>n;
+    for(i=1,i<=n,i++;;)
+    {
+        cout<<i;
+        i++;
+    };
+    return 0;
+}
