f7957bef46419a020683b048813938f7bcde653d
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2023-10-27 23:49:05 +0530

    Add files via upload

---
+++ 39.Factorial.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,n,fact;
+    cout<<"Enter Value of n ";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        fact=n*(n-1)
+    }
+    cout<<n<<"!="<<fact;
+    return 0;
+}
