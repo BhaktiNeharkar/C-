618f58fe9a0f00ccca57767216da150ace1c3f30
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2023-11-05 19:04:43 +0530

    Add files via upload

---
+++ 46.SumInArray.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,sum=0;
+    int A[i]={1,2,3,4,5,6,7,8,9};
+    for(int x:A)
+    {
+    sum=sum+x;
+    }
+    cout<<"Sum of all Elements is "<<sum;
+    return 0;
+}
