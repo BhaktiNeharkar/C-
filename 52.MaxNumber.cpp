5abe233dbf9fc35371ec86ed732fda497571b803
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2023-11-21 08:41:16 +0530

    Add files via upload

---
+++ 52.MaxNumber.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int A[]={4,6,27,15,31,22,30,29,8,16};
+    int max=A[0];
+    
+    for(int i=0;i<10;i++)
+    {
+        if(A[i]<max)
+        {
+            max=A[i];
+        }
+    }   
+    cout<<"Max No. is "<<max<<endl;
+    return 0;
+}
