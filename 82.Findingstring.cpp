5b6b8830a86c9a0e45db113d742a3ed926cd3854
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2024-03-09 23:17:54 +0530

    Add files via upload

---
+++ 82.Findingstring.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+    char a1[20]="programming";
+    char a2[10]="gram";
+    cout<<strstr(a1,a2)<<endl;
+    return 0; 
+}
