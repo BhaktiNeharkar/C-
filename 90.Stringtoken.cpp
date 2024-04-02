897c73cdd4722eb63e64c25e2e8b949f715a121e
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2024-04-02 23:10:11 +0530

    Add files via upload

---
+++ 90.Stringtoken.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <cstring>
+using namespace std;
+int main()
+{  
+    int x;
+    char s1[20]="345";
+    char s2[20]="46.56";
+
+    x=strtok(s1,=;);
+    
+    cout<<x;
+
+    return 0; 
+} 
