a0721b2d0d482f0138bca0e9575b28e954a3f842
Bhakti Neharkar <134431226+BhaktiNeharkar@users.noreply.github.com>
2024-04-18 23:43:48 +0530

    Update 105.Palindrome.cpp

--- 105.Palindrome.cpp
+++ 105.Palindrome.cpp
@@ -5,19 +5,20 @@ int main()
 {
     string str="MADAM";
     string rev=" ";
-
-    int len=(int)str.length();
+    int i,j;
+    int len;
+    len=(int)str.length();
     
     rev.resize(len); 
 
-    for(int i=0;int j=len-1;i<len; i++; j--)
+    for(i=0;j=len-1;i< len;i++;j--)
     {
        rev[i]=str[j];
     }
+
     rev[len]='\0';
 
     cout<<rev<<endl;
 
     return 0;
-
 }
